bd8392316ddb6e252a2da653d11d89f9412411b6
NAMIBANK\luxun <luxun@namibank.com>
2018-01-15 11:24:55 +0800

    Initial commit

---
+++ src/main/java/com/geekluxun/JcipApplication.java
@@ -0,0 +1,12 @@
+package com.geekluxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcipApplication.class, args);
+	}
+}
